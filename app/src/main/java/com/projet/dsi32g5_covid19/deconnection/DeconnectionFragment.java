31a0ce6374dbb4e7b0989e3d93ff2b983c6a2617
MokhtarHssine <mokhtar.hassine0103@gmail.com>
2021-01-02 12:38:48 +0100

    Implementer  activite log out

--- app/src/main/java/com/projet/dsi32g5_covid19/deconnection/DeconnectionFragment.java
+++ app/src/main/java/com/projet/dsi32g5_covid19/deconnection/DeconnectionFragment.java
@@ -1,6 +1,23 @@
 package com.projet.dsi32g5_covid19.deconnection;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.projet.dsi32g5_covid19.LoginActivity;
+
+
 public class DeconnectionFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        return null;
+    }
 }
