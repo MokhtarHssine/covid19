de44e5d3f3ba1254924b66a794bc03af74c00e9e
MokhtarHssine <mokhtar.hassine0103@gmail.com>
2021-01-03 01:07:03 +0100

    naviguer vers layout symptome

--- app/src/main/java/com/projet/dsi32g5_covid19/symptome/SymptomeFragment.java
+++ app/src/main/java/com/projet/dsi32g5_covid19/symptome/SymptomeFragment.java
@@ -1,6 +1,21 @@
 package com.projet.dsi32g5_covid19.symptome;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.projet.dsi32g5_covid19.R;
+
 public class SymptomeFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        final View root = inflater.inflate(R.layout.fragement_citez_symptomes, container, false);
+        return root;
+    }
 }
+
